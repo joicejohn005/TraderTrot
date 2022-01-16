83b0aa4c9ec9b8cfc77f9f1d09c9fc59616e81ca
joicejohn005 <joicejohn005@gmail.com>
2022-01-16 09:50:25 +0530

    login_tbl created

--- TraderTrot/app/models.py
+++ TraderTrot/app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class login_tbl(models.Model):
+    Name=models.CharField(max_length=30)
+    Email=models.CharField(max_length=30)
+    ContactNo=models.CharField(max_length=12)
+    ExperienceYr=models.CharField(max_length=2)
+    Profession=models.CharField(max_length=30)
+
+    class Meta:
+        db_table = "login_tbl"
