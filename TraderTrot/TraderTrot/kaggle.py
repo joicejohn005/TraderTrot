44a2dfef6f1531186ded3c74bb4ccad2cd24c980
joicejohn005 <joicejohn005@gmail.com>
2022-05-16 23:35:03 +0530

    new dell

--- TraderTrot/TraderTrot/kaggle.py
+++ TraderTrot/TraderTrot/kaggle.py
@@ -77,6 +77,8 @@ yf_returns = yf_returns.iloc[:, yf_returns.columns.get_level_values(1)=='Close']
 # yf_returns.tail(10)
 # print('shape: ', yf_returns.shape)
 
+#**********************************************************************************************#
+
 #yf_dividend = yf.Ticker(ticker[1]).dividends
 # print(dividends)
 
