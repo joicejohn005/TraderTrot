eb9aace36e76391a67721fd499e6d905858e7f81
joicejohn005 <joicejohn005@gmail.com>
2022-07-20 10:53:24 +0530

    addded procfile+configuration for heroku hosting+added domain name provided by heroku

--- TraderTrot/TraderTrot/urls.py
+++ TraderTrot/TraderTrot/urls.py
@@ -19,11 +19,17 @@ from app import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.decorators.csrf import csrf_exempt
-
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
     path('', views.index),
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+
     path('index/', views.index),
     path('blog/',views.blog),
     path('blogs/',views.blogs),
